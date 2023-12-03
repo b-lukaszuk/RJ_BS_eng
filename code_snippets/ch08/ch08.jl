165142b3424c60bec75271472f58de1ac2e24e2b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-12-03 15:34:25 +0100

    added initial code_snippets/ch08 content (*.toml files, *.jl file)

---
+++ code_snippets/ch08/ch08.jl
@@ -0,0 +1,9 @@
+###############################################################################
+#                                   imports                                   #
+###############################################################################
+import CairoMakie as Cmk
+import CSV as Csv
+import DataFrames as Dfs
+import GLM as Glm
+import RDatasets as RD
+import Statistics as Stats
