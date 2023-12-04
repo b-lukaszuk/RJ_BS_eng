183391f785f5433a7086cce4098976ff0c57dffb
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-12-04 20:15:37 +0100

    added code_snippets for ch08/Simple Linear Regression

--- code_snippets/ch08/ch08.jl
+++ code_snippets/ch08/ch08.jl
@@ -7,3 +7,55 @@ import DataFrames as Dfs
 import GLM as Glm
 import RDatasets as RD
 import Statistics as Stats
+
+
+###############################################################################
+#                           Simple Linear Regression                          #
+###############################################################################
+biomass = Csv.read("./biomass.csv", Dfs.DataFrame)
+first(biomass, 5)
+
+function getSlope(xs::Vector{<:Real}, ys::Vector{<:Real})::Float64
+    avgXs::Float64 = Stats.mean(xs)
+    avgYs::Float64 = Stats.mean(ys)
+    diffsXs::Vector{<:Real} = xs .- avgXs
+    diffsYs::Vector{<:Real} = ys .- avgYs
+    return sum(diffsXs .* diffsYs) / sum(diffsXs .^ 2)
+end
+
+function getIntercept(xs::Vector{<:Real}, ys::Vector{<:Real})::Float64
+    return Stats.mean(ys) - getSlope(xs, ys) * Stats.mean(xs)
+end
+
+# be careful unlike getCor or getCov, here the order of variables
+# in parameters influences the result
+plantAIntercept = getIntercept(biomass.rainL, biomass.plantAkg)
+plantASlope = getSlope(biomass.rainL, biomass.plantAkg)
+plantBIntercept = getIntercept(biomass.rainL, biomass.plantBkg)
+plantBSlope = getSlope(biomass.rainL, biomass.plantBkg)
+
+round.([plantASlope, plantBSlope], digits = 2)
+
+# Figure 35
+fig = Cmk.Figure()
+ax1, sc1 = Cmk.scatter(fig[1, 1], biomass.rainL, biomass.plantAkg,
+    markersize=25, color="skyblue", strokewidth=1, strokecolor="gray",
+    axis=(; title="Effect of rainfall on biomass of plant A",
+        xlabel="water [L]", ylabel="biomass [kg]")
+)
+ax2, sc2 = Cmk.scatter(fig[1, 2], biomass.rainL, biomass.plantBkg,
+    markersize=25, color="linen", strokewidth=1, strokecolor="black",
+    axis=(; title="Effect of rainfall on bomass of plant B",
+        xlabel="water [L]", ylabel="biomass [kg]")
+)
+Cmk.ablines!(fig[1, 1],
+    plantAIntercept,
+    plantASlope,
+    linestyle=:dash, color="gray")
+Cmk.ablines!(fig[1, 2],
+    plantBIntercept,
+    plantBSlope,
+    linestyle=:dash, color="gray")
+Cmk.linkxaxes!(ax1, ax2)
+Cmk.linkyaxes!(ax1, ax2)
+fig
